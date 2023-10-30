ac7cf337c198d84483f03351b848f037cffb07be
Mamatov Umar <mamatov.umar@mail.ru>
2023-10-30 15:50:53 +0500

    feat: header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+const { title } = defineProps<{
+  title: string
+}>()
+
+</script>
+
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="header__title">
+        {{ title }}
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+.header {
+  border-bottom: 1px solid var(--border-color);
+  height: 55px;
+  display: flex;
+  align-items: center;
+  margin-bottom: 30px;
+
+  &__title {
+    font-weight: 500;
+    font-size: 20px;
+  }
+}
+</style>
